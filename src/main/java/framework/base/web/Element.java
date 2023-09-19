14ce0588340a915fccf8c10f65c6ff1806dbd7ff
Zenric Navea <jazx.zn@gmail.com>
2023-09-19 15:47:32 +1200

    Code for parent element utilization and other verify

--- src/main/java/framework/base/web/Element.java
+++ src/main/java/framework/base/web/Element.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +16,13 @@ public class Element extends BaseTest {
     Logger Log = LoggerFactory.getLogger(Element.class);
     protected String description;
     protected By by;
+	protected Element parent;
+    
+	public Element(String description, By by, Element parent){
+		this.description = description;
+		this.by = by;
+		this.parent = parent;
+	}
 
     public Element(String description, By by) {
         this.description = description;
@@ -29,40 +34,87 @@ public class Element extends BaseTest {
      ********************************/
 
     public WebElement findElement() {
-        Assert.assertTrue(isExisting(), "Verify \"" + description + "\" is existing.");
-        return DriverManager.getDriver().findElement(this.by);
+        if (parent != null) {
+            return parent.findElement().findElement(this.by);
+        } else {
+            return DriverManager.getDriver().findElement(this.by);
+        }
     }
     
     public List<WebElement> findElements() {
-        return DriverManager.getDriver().findElements(this.by);
+        if (parent != null) {
+            return parent.findElement().findElements(this.by);
+        } else {
+            return DriverManager.getDriver().findElements(this.by);
+        }
+    }
+
+    public WebElement findParentElement() {
+        return parent.findElement();
     }
     
     public boolean isExisting() {
+        String methodDescription = getMethodName();
         boolean flag = findElements().size()!=0;
         if (flag) {
-            Log.info("\"" + description + "\" is existing.");
+            Log.debug(methodDescription + "\"{}\" is existing.", description);
         } else {
-            Log.info("\"" + description + "\" was not existing.");
+            Log.debug(methodDescription + "\"{}\" was not existing.", description);
         }
         return flag;
     }
 
     public WebElement waitElementIsVisible() {
-        return DriverManager.getWait().until(ExpectedConditions.visibilityOfElementLocated(this.by));
+        waitElementIsExisting();
+        DriverManager.getWait().until(d -> findElement().isDisplayed());
+        return findElement();
     }
 
-    public Boolean waitElementIsNotVisible() {
-        return DriverManager.getWait().until(ExpectedConditions.invisibilityOfElementLocated(this.by));
+    public WebElement waitElementIsVisible(int timeout) {
+        waitElementIsExisting(timeout);
+        DriverManager.getWait(timeout).until(d -> findElement().isDisplayed());
+        return findElement();
+    }
+
+    public void waitElementIsNotVisible() {
+        if (isExisting()) {
+            DriverManager.getWait().until(d -> findElement().isDisplayed()==false);
+        }
+    }
+    
+    public void waitElementIsNotVisible(int timeout) {
+        if (isExisting()) {
+            DriverManager.getWait(timeout).until(d -> findElement().isDisplayed()==false);    
+        }
+    }
+    
+    public WebElement waitElementIsExisting() {
+        DriverManager.getWait().until(d -> isExisting());
+        return findElement();
+    }
+
+    public WebElement waitElementIsExisting(int timeout) {
+        DriverManager.getWait(timeout).until(d -> isExisting());
+        return findElement();
+    }
+    
+    public void waitElementIsNotExisting() {
+        DriverManager.getWait().until(d -> isExisting()==false);
+    }
+    
+    public void waitElementIsNotExisting(int timeout) {
+        DriverManager.getWait(timeout).until(d -> isExisting()==false);
     }
 
     public boolean isDisplayed() {
-        Log.info("\"" + description + "\" is displayed.");
+        Log.info("\"{}\" is displayed.", description);
         return findElement().isDisplayed();
     }
 
     public void click() {
-        Log.info("\"" + description + "\" is clicked.");
-        findElement().click();
+        Log.info("\"{}\" is clicked.", description);
+        // findElement().click();
+        waitElementIsVisible().click();
     }
 
     public void enter(String value) {
@@ -72,19 +124,31 @@ public class Element extends BaseTest {
         } else {
             encryptedValue = value;   
         }
-        Log.info("\"" + encryptedValue + "\" is entered to \"" + description + "\".");
-        findElement().sendKeys(value);
+        Log.info("\"{}\" is entered to \"{}\".", encryptedValue, description);
+        // findElement().sendKeys(value);
+        waitElementIsVisible().sendKeys(value);
     }
 
     /*************************************
      ****** Verification - Assertion *****
      *************************************/
-
+    
     public void verifyIsVisible() {
-        String methodDescription = "Verify \"" + description + "\" is not visible.";
+        String methodDescription = "Verify \"" + description + "\" is visible.";
         ExtentTestManager.methodDescription = methodDescription;
         Log.info(methodDescription);
-        Assert.assertTrue(findElement().isDisplayed(), methodDescription);
+        waitElementIsVisible();
+        ExtentTestManager.passStep();
+    }
+
+    public void verifyIsVisible(int timeout) {
+        String methodDescription = "Verify \"" + description + "\" is visible.";
+        ExtentTestManager.methodDescription = methodDescription;
+        Log.info(methodDescription);
+        if (timeout > 0) {
+            Log.info("Timeout: {} seconds", timeout );
+        }
+        waitElementIsVisible(timeout);
         ExtentTestManager.passStep();
     }
 
@@ -92,19 +156,37 @@ public class Element extends BaseTest {
         String methodDescription = "Verify \"" + description + "\" is not visible.";
         Log.info(methodDescription);
         ExtentTestManager.methodDescription = methodDescription;
-        Boolean flag = isExisting();
-        if (flag) {
-            flag = findElement().isDisplayed();
-            Assert.assertFalse(flag, methodDescription);
-        }
+        waitElementIsNotVisible();
         ExtentTestManager.passStep();
     }
     
+    public void verifyIsNotVisible(int timeout) {
+        String methodDescription = "Verify \"" + description + "\" is not visible.";
+        Log.info(methodDescription);
+        ExtentTestManager.methodDescription = methodDescription;
+        if (timeout > 0) {
+            Log.info("Timeout: {} seconds", timeout );
+        }
+        waitElementIsNotVisible(timeout);
+        ExtentTestManager.passStep();
+    }
+
     public void verifyIsExisting() {
         String methodDescription = "Verify \"" + description + "\" is existing.";
         ExtentTestManager.methodDescription = methodDescription;
         Log.info(methodDescription);
-        Assert.assertTrue(isExisting(), methodDescription);
+        waitElementIsExisting();
+        ExtentTestManager.passStep();
+    }
+    
+    public void verifyIsExisting(int timeout) {
+        String methodDescription = "Verify \"" + description + "\" is existing.";
+        ExtentTestManager.methodDescription = methodDescription;
+        Log.info(methodDescription);
+        if (timeout > 0) {
+            Log.info("Timeout: {} seconds", timeout );
+        }
+        waitElementIsExisting(timeout);
         ExtentTestManager.passStep();
     }
     
@@ -112,7 +194,18 @@ public class Element extends BaseTest {
         String methodDescription = "Verify \"" + description + "\" is not existing.";
         ExtentTestManager.methodDescription = methodDescription;
         Log.info(methodDescription);
-        Assert.assertFalse(isExisting(), methodDescription);
+        waitElementIsNotExisting();
+        ExtentTestManager.passStep();
+    }
+    
+    public void verifyIsNotExisting(int timeout) {
+        String methodDescription = "Verify \"" + description + "\" is not existing.";
+        ExtentTestManager.methodDescription = methodDescription;
+        Log.info(methodDescription);
+        if (timeout > 0) {
+            Log.info("Timeout: {} seconds", timeout );
+        }
+        waitElementIsNotExisting(timeout);
         ExtentTestManager.passStep();
     }
 
