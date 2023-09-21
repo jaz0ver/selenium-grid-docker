cd9f8aead6ac3554757696048350f6619837f56f
Zenric Navea <jazx.zn@gmail.com>
2023-09-21 13:00:29 +1200

    Update for handling invalid selenium option

--- src/main/java/framework/base/Driver.java
+++ src/main/java/framework/base/Driver.java
@@ -29,47 +29,50 @@ public class Driver {
         String methodName = CommonFunctions.getMethodName();
         DesiredCapabilities dc = new DesiredCapabilities();
         try {
-            seleniumType = seleniumType.trim();
-            if (seleniumType.equalsIgnoreCase("grid")) {
-                URL url = new URL(ConfigFileReader.getProperty("grid_hub"));
-                switch (browser) {
-                    case "edge":
-                        dc.acceptInsecureCerts();
-                        dc.setBrowserName("MicrosoftEdge");
-                        break;
-                    case "firefox":
-                        dc.setBrowserName("firefox");
-                        break;
-                    default:
-                        Log.warn(methodName + "No specific browser was set. " + browser);
-                    case "chrome":
-                        dc.setBrowserName("chrome");
-                        break;
-                }
-                DriverManager.setDriver(new RemoteWebDriver(url, dc));
-            } else if(seleniumType.equalsIgnoreCase("webDriver")) {
-                switch (browser) {
-                    case "edge":
-                        WebDriverManager.edgedriver().setup();
-                        DriverManager.setDriver(new EdgeDriver());
-                        break;
-                    case "firefox":
-                        WebDriverManager.firefoxdriver().setup();
-                        DriverManager.setDriver(new FirefoxDriver());
-                        break;
-                    case "chrome_debug":
-                        WebDriverManager.chromedriver().setup();
-                        DriverManager.setDriver(new ChromeDriver(chromeDebug()));
-                        break;
-                    default:
-                        Log.warn(methodName + "No specific browser was set. " + browser);
-                    case "chrome":
-                        WebDriverManager.chromedriver().setup();
-                        DriverManager.setDriver(new ChromeDriver());
-                        break;
-                }
-            } else {
-                Log.error(methodName + "Selenium type was not specified");
+            seleniumType = seleniumType.trim().toLowerCase();
+            switch (seleniumType) {
+                case "grid":
+                    URL url = new URL(ConfigFileReader.getProperty("grid_hub"));
+                    switch (browser) {
+                        case "edge":
+                            dc.acceptInsecureCerts();
+                            dc.setBrowserName("MicrosoftEdge");
+                            break;
+                        case "firefox":
+                            dc.setBrowserName("firefox");
+                            break;
+                        default:
+                            Log.warn("{}Browser selected was not in the list({}). Defaulting to Chrome browser.", methodName, browser);
+                        case "chrome":
+                            dc.setBrowserName("chrome");
+                            break;
+                    }
+                    DriverManager.setDriver(new RemoteWebDriver(url, dc));
+                    break;
+                default:
+                    Log.warn("{}Selenium type was not specified (Option: Grid or WebDriver). Defaulting to Selenium WebDriver.", methodName);
+                case "webdriver":
+                    switch (browser) {
+                        case "edge":
+                            WebDriverManager.edgedriver().setup();
+                            DriverManager.setDriver(new EdgeDriver());
+                            break;
+                        case "firefox":
+                            WebDriverManager.firefoxdriver().setup();
+                            DriverManager.setDriver(new FirefoxDriver());
+                            break;
+                        case "chrome_debug":
+                            WebDriverManager.chromedriver().setup();
+                            DriverManager.setDriver(new ChromeDriver(chromeDebug()));
+                            break;
+                        default:
+                            Log.warn("{}Browser selected was not in the list({}). Defaulting to Chrome browser.", methodName, browser);
+                        case "chrome":
+                            WebDriverManager.chromedriver().setup();
+                            DriverManager.setDriver(new ChromeDriver());
+                            break;
+                    }
+                    break;
             }
             Capabilities cap = DriverManager.getCapabilities();
             Log.info("Device details: " + cap.getBrowserName().toUpperCase() + " " + cap.getBrowserVersion());
